8a31f1bdb8dba1c8d95b24be2efc3803f24a2605
lenard <8lenard@gmail.com>
2026-02-24 19:40:17 +0800

    update

--- src/settings/settings.jsx
+++ src/settings/settings.jsx
@@ -4,7 +4,7 @@ function settings() {
   return (
     <div className='fixed right-0 '>settings
     <h1>Hello</h1>
-    <h1>hello</h1>
+    <h1>hi</h1>
     {/*git pull origin main*/}
     </div>
   )
