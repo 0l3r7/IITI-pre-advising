e1fd3cf7a51ee5f9bf754122e1c959c07b54aebe
Justine Deguzman <justinedeguzman616@gmail.com>
2026-02-24 19:32:30 +0800

    first try of collab

--- src/settings/settings.jsx
+++ src/settings/settings.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function settings() {
   return (
-    <div className='fixed right-0 '>settings</div>
+    <div className='fixed right-0 '>settings
+    <h1>Hello</h1>
+    {/*git pull origin main*/}
+    </div>
   )
 }
 
